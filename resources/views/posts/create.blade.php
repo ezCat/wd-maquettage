797633c38b7ffa99f92303cf876527a1e229ef7d
Dennis <dennis_wittmer@yahoo.fr>
2016-04-13 22:39:39 +0200

    New form CSS

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,56 +3,86 @@
 @section('content')
 
     <div class="page-wrapper">
+        <div class="container">
 
-    <div class="row">
-        <h1><span class="important">AJOUTES</span> TON EXERCICE</h1>
-    </div>
-
-    <div class="row">
-
-        <div class="col-md-4">
-            <img src="{{ asset('public/img/mail.svg')}}" width="80%" height="100%">
-        </div>
-
-        <div class="col-md-8">
+            <div class="row text-center">
+                <h1>AJOUTES TON <span class="important"> EXERCICE </span>!</h1>
+            </div>
 
             {!! Form::open(['url' => route('posts.store')]) !!}
-            <br><br>
 
-            <span class="btn btn-circle-title" style="float: left;">1</span> 
-            <h3 style="margin-top: 7px;">Définissez l'exercice</h3>
+            <div class="row space-under">
+                <span class="btn btn-circle-title" style="float: left;">1</span>
+                <h3 style="margin-top: 7px;">Décris l'exercice</h3>
 
-            <br><br>
+            </div>
 
-            {!! Form::label('', 'Titre') !!}
-            {!! Form::text('titre', null, ['class' => 'form-control']) !!}<br><br>
+            <div class="row space-under">
+                <div class="input-group space-under">
+                    <div class="input-group-btn">
+                        <button type="button" class="btn btn-danger">Titre</button>
+                    </div>
+                    {!! Form::text('titre', null, ['class' => 'form-control', 'placeholder' => "Veuillez rentrer le titre de l'article"]) !!}
+                </div>
+                {!! Form::textarea('description', null, ['class' => 'form-control', 'placeholder' => 'Veuillez rentrer la description de l'article']) !!}
+            </div>
 
-            {!! Form::label('', 'Catégorie') !!}
-            {!! Form::select('id_categorie', $categories, null,['class' => 'form-control']) !!}<br><br>
+            <div class="row space-under">
+                <span class="btn btn-circle-title" style="float: left;">2</span>
+                <h3 style="margin-top: 7px;">Ajoutes la vidéo</h3>
+            </div>
 
-            {!! Form::label('', 'Site d\'origine de la vidéo') !!}<br>
-            {!! Form::radio('id_type_video', 1) !!} <i class="fa fa-youtube"></i> YouTube<br>
-            {!! Form::radio('id_type_video', 2) !!} <i class="fa fa-vine"></i> Vine<br>
-            {!! Form::radio('id_type_video', 3) !!} <i class="fa fa-instagram"></i> Instagram<br>
+            <div class="row">
+                <div class="col-md-4 col-ld-4 col-xs-12 text-center red video-box">
+                    <i class="fa fa-thumbs-up three-logo-column"></i>
+                    <h4>Thumbs Up !</h4>
+                </div>
 
-            <br>
+                <div class="col-md-4 col-ld-4 col-xs-12 text-center green bordering video-box">
+                    <i class="fa fa-thumbs-up three-logo-column"></i>
+                    <h4>Thumbs Up !</h4>
+                </div>
 
-            {!! Form::label('', 'URL de la vidéo') !!}
-            {!! Form::text('url', null, ['class' => 'form-control', 'placeholder' => '']) !!}<br><br>
+                <div class="col-md-4 col-ld-4 col-xs-12 text-center blue video-box">
+                    <i class="fa fa-thumbs-up three-logo-column"></i>
+                    <h4>Thumbs Up !</h4>
+                </div>
+            </div>
 
-            {!! Form::label('', 'URL du schéma') !!}
-            {!! Form::text('image', null, ['class' => 'form-control', 'placeholder' => '']) !!}<br><br>
+            <div class="row">
+                <div class="col-md-12 col-ld-12 col-xs-12 text-center blue video-box">
 
-            {!! Form::label('', 'Description') !!}
-            {!! Form::textarea('description', null, ['class' => 'form-control', 'placeholder' => 'Cet exercice a pour objectif de..']) !!}
-            <br><br>
+                    <div class="input-group space-under">
+                        <div class="input-group-btn">
+                            <button type="button" class="btn btn-danger">URL</button>
+                        </div>
+                        {!! Form::text('url', null, ['class' => 'form-control', 'placeholder' => 'Rentrer l\'URL de la vidéo ici']) !!}
+                    </div>
+                </div>
+            </div>
 
-            <button class="btn btn-primary">Envoyer</button>
+            <div class="block-hidden">
+                {!! Form::radio('id_type_video', 1) !!} <i class="fa fa-youtube"></i> YouTube<br>
+                {!! Form::radio('id_type_video', 2) !!} <i class="fa fa-vine"></i> Vine<br>
+                {!! Form::radio('id_type_video', 3) !!} <i class="fa fa-instagram"></i> Instagram<br>
+            </div>
 
-            {!! Form::close() !!}
+            <div class="row space-under">
+                <span class="btn btn-circle-title" style="float: left;">3</span>
+                <h3 style="margin-top: 7px;">L'exercice est-il difficile ?</h3>
+            </div>
 
-        </div>
+            <h4 class="space-under">Difficulté de l'exercice :</h4>
+
+            <div id="slider-default" class="slider-info"></div>
+
+            <div class="block-hidden">
+                <input type="text" id="value-slider">
+            </div>
+
     </div>
     </div>
+
+    {!! Form::close() !!}
 
 @endsection
